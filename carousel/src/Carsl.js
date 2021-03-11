45eda04e3dc455b925db6e4e9c3f56871e76c4b0
Thies Bucker <thiesbert@yahoo.de>
2021-03-11 10:31:53 +0100

    Added the carussel

--- carousel/src/Carsl.js
+++ carousel/src/Carsl.js
@@ -1,31 +1,40 @@
-import React,{Component} from 'react';
+import React from 'react';
 import './App.css';
- 
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 let styles = {
-    margin: 'auto',
-    width: '500px'
-  };
+  margin: 'auto',
+  width: '500px'
+};
 
-  /* Use this data to create an array of objects that you loop through
-  to create the carousel. 
-
-  Hong Kong: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg
-  Singapore: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp
-  Japan: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp
-  Las Vegas: https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp
-
-  Use the react-responsive-carousel package to create the Carousel: 
-  https://www.npmjs.com/package/react-responsive-carousel
+/* 
+Use the react-responsive-carousel package to create the Carousel: 
+https://www.npmjs.com/package/react-responsive-carousel
 
 
-  */
+*/
 
 const Carsl = () => {
-	return (
-		<div style={styles}>
-			 Welcome to the Carousel..
-		</div>
-	  )
+  return (
+    <div style={styles}>
+      Welcome to the Carousel
+      <Carousel>
+        <div>
+          <img alt="Hong Kong" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" />
+        </div>
+        <div>
+          <img alt="Singapore" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" />
+        </div>
+        <div>
+          <img alt="Japan" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" />
+        </div>
+        <div>
+          <img alt="Las Vegas" src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" />
+        </div>
+      </Carousel>
+    </div>
+  )
 }
 export default Carsl;
